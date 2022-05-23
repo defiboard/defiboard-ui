1d304042330f1745ad53b5aa8430c40d1223fbc8
Manhar Shukla <xcryptoshadow@gmail.com>
2022-05-23 20:53:52 +0530

    swig api

--- web/assets/js/wallet/swing.js
+++ web/assets/js/wallet/swing.js
@@ -1,10 +1,43 @@
+function formatParams(params) {
+  Object.keys(params)
+    .map(
+      (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+    )
+    .join("&");
+}
+async function getQuotes(params) {
+  const data = fetchSwingData(params);
+  const itemsData = data.routes;
+  for (let i = 0; i < itemsData.length; i++) {
+    let item = itemsData[i];
+    try {
+      let x = `<tr>  
+      <td> <img src="${item.token_0.logo_url}" style="width:50px" alt="" />  </td>
+            
+            <td> ${item[i].duration} </td>
+            <td> ${item[i].amount} </td>
+            <td> ${item[i].bridgeFee} </td>
+            <td> ${item[i].bridge} </td>
+        
+            
+            </tr>`;
 
-
-
-
-async function fetchSwingData( params) {
-    return request(
-      "GET",
-      `https://swap.dev.swing.xyz/v0/transfer/quote?${formatParams(params)}`
-     );
+      body = body + x;
+    } catch (error) {
+      console.log(error);
+    }
   }
+  divPagePools.classList.remove("hidden");
+  poolsBody.innerHTML = body;
+}
+
+async function fetchSwingData(params) {
+  return request(
+    "GET",
+    `https://swap.dev.swing.xyz/v0/transfer/quote?${formatParams(params)}`
+  );
+}
+
+getQuote.addEventListener("click", async () => {
+  alert("get quote clicked");
+});
